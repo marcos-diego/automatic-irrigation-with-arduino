04106a9aa30f46838c2250e2f737198839fda4b1
Marcos Santos <62906467+marcos-diego@users.noreply.github.com>
2020-03-30 15:36:33 -0500

    Update automatic_irrigation_code.ino

--- src/automatic_irrigation_code.ino
+++ src/automatic_irrigation_code.ino
@@ -1,20 +1,13 @@
-/* ====================================================================================================================
-    Project: Automatic Watering System
-    Developers coder: Marcos Santos and André Souza
-    Last update: September 14, 2019
-====================================================================================================================== */
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Defines the pins that will be used to connect to the lcd display
 LiquidCrystal_I2C lcd(0x27, 16, 2, POSITIVE);
 
-#define sensor_pin A0        // define sensor pin
-#define red_LED_pin 9        // define red LED pin
-#define blue_LED_pin 10      // define blue LED pin
-#define green_LED_pin 12     // define green LED pin
-#define relay_waterpump 8    // define water pump pin
+#define sensor_pin A0
+#define red_LED_pin 9
+#define blue_LED_pin 10
+#define green_LED_pin 12
+#define relay_waterpump 8
  
 void setup()
 {
@@ -39,13 +32,13 @@ void loop()
   lcd.clear();
  
   // Displays the value of analog port on the serial monitor
-  Serial.print("Porta analogica: ");
+  Serial.print("Analog port: ");
   Serial.print(analog_value);
 
   // Displays the percentage value of analog port on the LCD display
   lcd.setBacklight(HIGH);
   lcd.setCursor(13, 0);
-  lcd.print("valor_analogico");
+  lcd.print("analog value");
   lcd.print("%");
 
   //=================================================================================
@@ -53,16 +46,16 @@ void loop()
   // Moist soil, lights the green LED
   if (analog_value > 50)
   {
-    Serial.println(" Status: Solo úmido");
+    Serial.println("Moist soil");
     lowLEDs();
     digitalWrite(green_LED_pin, HIGH);
 
     // Display moisture status on the LCD display
     lcd.setBacklight(HIGH);
     lcd.setCursor(0, 0);
-    lcd.print("Solo úmido");
+    lcd.print("Moist soil");
     lcd.setCursor(0, 1);
-    lcd.print("Bomba desligada");
+    lcd.print("Pump off");
     digitalWrite(relay_waterpump, LOW);
   }
 
@@ -71,16 +64,16 @@ void loop()
   // Soil with moderate humidity, blue LED light
   if (analog_value < 50 && analog_value > 30)
   {
-    Serial.println(" Status: Humidade Moderada");
+    Serial.println("moderate humidity");
     lowLEDs();
     digitalWrite(blue_LED_pin, HIGH);
 
     // Display moisture status on the LCD display
     lcd.setBacklight(HIGH);
     lcd.setCursor(0, 0);
-    lcd.print("Humidade Moderada");
+    lcd.print("moderate humidity");
     lcd.setCursor(0, 1);
-    lcd.print("Bomba desligada");
+    lcd.print("Pump off");
     digitalWrite(relay_waterpump, LOW);
   }
 
@@ -89,19 +82,16 @@ void loop()
   // Dry soil, red LED light
   if (analog_value < 20 && analog_value >= 0)
   {
-    Serial.println(" Status: Solo seco");
+    Serial.println("Dry soil");
     lowLEDs();
     digitalWrite(red_LED_pin, HIGH);
 
     lcd.setCursor(0, 0);
     lcd.setBacklight(HIGH);
-    lcd.print("Solo seco");
+    lcd.print("Dry soil");
     lcd.setCursor(0, 1);
-    lcd.print("Bomba ligada");
+    lcd.print("Pump off");
     digitalWrite(relay_waterpump, HIGH);
-    //delay(1000);
-    //digitalWrite(relay_waterpump, LOW);
-    //delay(5000);
   }
   delay(1000);
   
